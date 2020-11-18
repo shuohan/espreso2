cfff97067fa3cfa56f22d9629a9339b14a0ca329
Shuo Han <shan50@jhu.edu>
2020-11-18 12:16:02 -0500

    Plot loss or evaluation curves

---
+++ scripts/eval/plot_curves.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input-files', nargs='+')
+parser.add_argument('-n', '--names', nargs='+')
+parser.add_argument('-m', '--markers', nargs='+', type=int)
+parser.add_argument('-x', '--x-column-name')
+parser.add_argument('-y', '--y-column-name')
+parser.add_argument('-o', '--output-file')
+args = parser.parse_args()
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from plotting_utils import get_colors
+
+
+colors = get_colors(10, 'tab10')
+assert len(args.input_files) <= len(colors)
+
+if args.names is not None:
+    assert len(args.names) == len(args.input_files)
+
+fig = plt.figure()
+for fn, color in zip(args.input_files, colors):
+    df = pd.read_csv(fn)[[args.x_column_name, args.y_column_name]]
+    plt.plot(df[args.x_column_name], df[args.y_column_name], color=color)
+
+
+if args.names is not None:
+    plt.legend(args.names)
+
+if args.markers is not None:
+    y = plt.gca().get_ylim()[1]
+    ys = [y] * len(args.markers)
+    print(args.markers, ys)
+    plt.stem(args.markers, ys, linefmt='k--', markerfmt=' ', basefmt=' ')
+
+plt.grid(True)
+fig.savefig(args.output_file)
