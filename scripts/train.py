8b12a3ad164fe1e38d66e9e4655dc76338b99144
Shuo Han <shan50@jhu.edu>
2020-11-28 10:05:49 -0500

    Specify each kernel size in disc

--- scripts/train.py
+++ scripts/train.py
@@ -22,8 +22,8 @@ parser.add_argument('-sw', '--smoothness-loss-weight', default=1.0, type=float)
 parser.add_argument('-z', '--z-axis', default=2, type=int)
 parser.add_argument('-isz', '--image-save-zoom', default=1, type=int)
 parser.add_argument('-wd', '--weight-decay', default=0, type=float)
-parser.add_argument('-lrdk', '--lrd-kernel-size', default=(3, 1),
-                    type=int, nargs=2)
+parser.add_argument('-lrdk', '--lrd-kernels', default=(3, 3, 3, 3, 3),
+                    nargs='+', type=int)
 args = parser.parse_args()
 
 
