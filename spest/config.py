bc250ce4572a29464505bdeac2e3ca1d49920a9b
Shuo Han <shan50@jhu.edu>
2020-12-02 22:43:48 -0500

    Add option to use zero-padded kernel net

--- spest/config.py
+++ spest/config.py
@@ -48,3 +48,4 @@ class Config(Config_):
         self.add_config('learning_rate', 1e-4)
         self.add_config('num_epochs_per_stage', 4000)
         self.add_config('symm_kernel', False)
+        self.add_config('zero_pad_kn', False)
--- tests/test_train.sh
+++ tests/test_train.sh
@@ -18,9 +18,9 @@ for image in ${images[@]}; do
     fwhm=$(echo $image | sed "s/.*\(fwhm-.*\)_scale.*/\1/")
     scale=$(echo $image | sed "s/.*\(scale-.*\)_len.*/\1/")
     len=$(echo $image | sed "s/.*\(len-.*\)\.nii/\1/")
-    outdir=../tests/results_avg/oasis3_${fwhm}_${scale}_${len}_ns-${ns}_flip_sw-${sw}_sigma-1_symm
+    outdir=../tests/results_zp/oasis3_${fwhm}_${scale}_${len}_ns-${ns}_flip_sw-${sw}_sigma-1_no-zp
     kernel=$(echo $image | sed "s/\.nii/_kernel.npy/")
-    docker run --gpus device=1 --rm \
+    docker run --gpus device=0 --rm \
         -v $psf_est_dir:$psf_est_dir \
         -v $sssrlib_dir:$sssrlib_dir \
         -v $proc_dir:$proc_dir \
@@ -34,7 +34,7 @@ for image in ${images[@]}; do
         pytorch-shan:1.7.0-cuda11.0-cudnn8-runtime \
         ./train.py -i $image -o $outdir -k $kernel -kl 21 -isz 10 -e 10000 \
         -sw ${sw} -wd 1e-2 -lrdk 3,1 3,1 3,1 1,1 1,1 1,1 1,1 1,1 1,1 1,1 \
-        -lrdc 64 64 64 64 64 64 64 64 64 -knc 6 -ns ${ns} -ps 16 -ie ${ie} -sk
+        -lrdc 64 64 64 64 64 64 64 64 64 -knc 6 -ns ${ns} -ps 16 -ie ${ie}
         # -lrdc 128 128 128 128 128 128 128 128 128
 
 done # | rush -j 3 {}
