d4f2ff3c61275649f95ac4542b1461263b4f246f
Shuo Han <shan50@jhu.edu>
2020-12-04 08:36:34 -0500

    Do some tests

--- tests/test_train.sh
+++ tests/test_train.sh
@@ -8,17 +8,27 @@ trainer_dir=~/Code/shuo/deep-networks/pytorch-trainer
 simu_dir=~/Code/shuo/utils/lr-simu
 data_dir=/data
 
-images=(/data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-2p0_scale-0p125_len-13.nii)
+images=(/data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-2p0_scale-0p125_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-4p0_scale-0p125_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-8p0_scale-0p125_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-2p0_scale-0p25_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-4p0_scale-0p25_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-8p0_scale-0p25_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-2p0_scale-0p5_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-4p0_scale-0p5_len-13.nii
+        /data/oasis3/simu/sub-OAS30001_ses-d0129_acq-mprage_run-01_T1w_type-gauss_fwhm-8p0_scale-0p5_len-13.nii)
+
 
 ns=1
 sw=0
 ie=200
+wd=5e-2
 
 for image in ${images[@]}; do
     fwhm=$(echo $image | sed "s/.*\(fwhm-.*\)_scale.*/\1/")
     scale=$(echo $image | sed "s/.*\(scale-.*\)_len.*/\1/")
     len=$(echo $image | sed "s/.*\(len-.*\)\.nii/\1/")
-    outdir=../tests/results_try-neg/oasis3_${fwhm}_${scale}_${len}_ns-${ns}_flip_sw-${sw}_ie-${ie}
+    outdir=../tests/results_try-neg/oasis3_${fwhm}_${scale}_${len}_ns-${ns}_flip_sw-${sw}_ie-${ie}_wd-${wd}
     kernel=$(echo $image | sed "s/\.nii/_kernel.npy/")
     docker run --gpus device=1 --rm \
         -v $psf_est_dir:$psf_est_dir \
@@ -32,8 +42,8 @@ for image in ${images[@]}; do
         -e PYTHONPATH=$psf_est_dir:$sssrlib_dir:$proc_dir:$trainer_dir:$config_dir:$simu_dir \
         -w $psf_est_dir/scripts -t \
         pytorch-shan:1.7.0-cuda11.0-cudnn8-runtime \
-        ./train.py -i $image -o $outdir -k $kernel -kl 21 -isz 4 -e 8000 \
-        -sw ${sw} -wd 1e-2 -lrdk 3,1 3,1 3,1 1,1 1,1 1,1 1,1 1,1 1,1 1,1 \
+        ./train.py -i $image -o $outdir -k $kernel -kl 21 -isz 4 -e 10000 \
+        -sw ${sw} -wd ${sw} -lrdk 3,1 3,1 3,1 1,1 1,1 1,1 1,1 1,1 1,1 1,1 \
         -lrdc 64 64 64 64 64 64 64 64 64 -knc 6 -ns ${ns} -ps 16 -ie ${ie}
         # -lrdc 128 128 128 128 128 128 128 128 128
 
